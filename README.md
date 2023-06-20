bc7d50985f8cacfdf606496d87aeb290e398e549
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-20 19:16:47 +0400

    add ssg

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ![Banner](./Banner.png)
 
 A personal project to use IconSax icons in Notion.
+
+## Static Site Generator (Node.js)
+
+```
+npm run build.server
+```
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,57 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { $, component$ } from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+export const useFiles = routeLoader$(async () => {
+  // FIXME: Relative path does not work!!!
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const iconsDir = path.resolve(dirname, "../icons");
+
+  const files = fs.readdirSync(iconsDir);
+  const svgs = files
+    .filter((f) => path.extname(f) === ".svg")
+    .map((svg) => fs.promises.readFile(path.resolve(iconsDir, svg), "utf-8"));
+  return Promise.all(svgs);
+});
 
 export default component$(() => {
+  const svgsSignal = useFiles();
+
+  const handleCopy = $((text: string) => {
+    const base64 = btoa(text);
+    const type = "text/plain";
+    const blob = new Blob([`data:image/svg+xml;base64,${base64}`], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+
+    navigator.clipboard.write(data).then(
+      () => {
+        alert("Thank you!");
+      },
+      () => {
+        alert("Shit!");
+      }
+    );
+  });
+
   return (
-    <div>
+    <>
       <h1>Icons</h1>
-    </div>
+      <div class="container">
+        {svgsSignal.value.map((svg) => (
+          <button
+            onClick$={() => {
+              handleCopy(svg);
+            }}
+            class="icon-button"
+            key={svg}
+          >
+            <div dangerouslySetInnerHTML={svg} />
+          </button>
+        ))}
+      </div>
+    </>
   );
 });
 
