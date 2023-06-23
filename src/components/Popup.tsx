068a35fefc6987e6efdf68d3f0351dc9a5748176
Alireza Safaierad <frontendmonster@gmail.com>
2023-06-24 01:56:05 +0400

    add Popup component

---
+++ src/components/Popup.tsx
@@ -0,0 +1,55 @@
+import {
+  useSignal,
+  useVisibleTask$,
+  type Signal,
+  component$,
+  $,
+} from '@builder.io/qwik';
+import { css } from 'pandacss';
+
+interface Props {
+  ref: Signal<any>;
+}
+
+export const Popup = component$(({ ref }: Props) => {
+  const inputRef = useSignal<Element | undefined>();
+
+  useVisibleTask$(() => {
+    ref.value = {
+      showPopup: $(function showPopup(text: string, time = 1000) {
+        if (!inputRef.value) return;
+
+        inputRef.value.textContent = text;
+        inputRef.value.classList.add('show');
+        setTimeout(() => {
+          inputRef.value?.classList.remove('show');
+        }, time);
+      }),
+    };
+  });
+  return (
+    <div
+      ref={inputRef}
+      id="popup"
+      class={css({
+        background: 'highlight',
+        fontWeight: '400',
+        borderRadius: 'md',
+        padding: 4,
+        color: 'brand',
+        position: 'fixed',
+        bottom: 0,
+        opacity: 0,
+        left: '50%',
+        translate: '-50% 0',
+        margin: '0 auto',
+        transitionProperty: 'opacity, bottom',
+        transitionDuration: '100ms',
+        transitionTimingFunction: 'ease-in',
+        boxShadow: 'xl',
+      })}
+    >
+      Copied!
+    </div>
+  );
+});
