62404dece0127d5771376a0bad09bdc55ea4602a
ASafaierad <frontendmonster@gmail.com>
2024-07-22 02:29:14 +0200

    add base to vite

--- docs/build/q-BL75_pz_.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./q-uemlvruI.js";import{B as r,q as t}from"./q-CN17xajZ.js";const e=r(t(()=>o(()=>import("./q-LcYr7ozf.js"),[]),"s_J3jpcJ8Bvak"));export{e as default};
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    // base: '/notion-icons',
+    base: '/notion-icons',
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
